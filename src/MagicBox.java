0f1b08c6a7099ee23d95bb21b55f8afe7d25bb7d
Dmitriy Kyrov <kdg149@yandex.ru>
2022-08-11 16:57:18 +0300

    added empty methods

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,2 +1,20 @@
-public class MagicBox {
+public class MagicBox<T> {
+
+    private T number;
+
+    public MagicBox(T number) {
+        this.number = number;
+    }
+
+    public T getNuber() {
+        return number;
+    }
+
+    boolean add(T item) {
+        return true;
+    }
+
+    T pick() {
+        return null;
+    }
 }
